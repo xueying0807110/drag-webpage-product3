9a54e4259e17b44e423f776683cb6aa2278db2d2
xueying0807110 <xueying0807110@gmail.com>
2026-06-11 20:33:25 +0800

    Add files via upload

---
+++ products.js
@@ -0,0 +1,14 @@
+window.PRODUCTS = [
+  {
+    "name": "Black Gold Marbling",
+    "image": "product-images/Black Gold Marbling.png"
+  },
+  {
+    "name": "Blue Cracked Marbling",
+    "image": "product-images/Blue Cracked Marbling.png"
+  },
+  {
+    "name": "Blue Marbling",
+    "image": "product-images/Blue Marbling.png"
+  }
+];
